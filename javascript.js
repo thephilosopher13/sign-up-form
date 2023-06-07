bfbdc0d7327c17da4f28ce52a8fa10894d7229fe
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-06-07 16:55:53 +0800

    Finish Javascript Functionality for password confirmation

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const password = document.getElementById('pwd');
+    const confirmPassword = document.getElementById('confirmpwd');
+    const confirmPasswordError = document.getElementById('confirmpwderror');
+    const errorspans = document.querySelectorAll("span");
+    const form = document.getElementById('signupform');
+
+    
+
+    function checkPasswordMatch() {
+        if (password.value !== confirmPassword.value) {
+        confirmPasswordError.classList.add('invalid');
+    } else {
+        confirmPasswordError.classList.remove('invalid');
+    }
+}
+
+    form.addEventListener('submit', (e) => {
+        if (confirmPasswordError.classList.contains("invalid")) {
+            e.preventDefault(); // Prevent form submission
+
+        }
+    });
+
+    password.addEventListener('input', checkPasswordMatch);
+    confirmPassword.addEventListener('input', checkPasswordMatch);
+
+});
 
